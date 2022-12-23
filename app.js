04cb9d8a191ca50230ad246eaa6160f83d34bf71
VardanAntanosyan06 <vantanosyan99@gmail.com>
2022-12-23 12:14:46 +0400

    commit for reg/login

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/register', usersRouter);
-app.use(' ', verifyRouter);
+app.use('/verify', verifyRouter);
 app.use('/login', loginRouter);
 
 
