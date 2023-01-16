e5d8bbda372de02d4383296d43e049b08992893f
VardanAntanosyan06 <vantanosyan99@gmail.com>
2023-01-16 11:16:05 +0400

    just commit

--- controllers/getTaskController.js
+++ controllers/getTaskController.js
@@ -49,7 +49,7 @@ const getTasksInCalendar = async (req,res)=>{
     try {
         const {authorization: token} = req.headers;
       const user = await UserModel.findOne({where:{token: token.replace('Bearer ', '')}})
-    const task = await Calendar.findAll({where:{userId:user.id},include:[TaskModel]})
+    const task = await Calendar.findAll({where:{userId:user.id},include:[TaskModel,SubTasks]})
             if (task) {
                 return res.status(200).json({task})
             }
