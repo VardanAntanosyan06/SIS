a949e7e5bd75e3d54d42f1398828eccef66abca1
VardanAntanosyan06 <vantanosyan99@gmail.com>
2023-01-16 04:26:39 +0400

    just commit

--- controllers/regController.js
+++ controllers/regController.js
@@ -6,58 +6,215 @@ require("dotenv").config();
 const model = require("../models").Users;
 const reg = async (req, res) => {
   try {
-    // console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
-    // console.log(req.body);
-    // console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-    const {fullName,email,password,phone,age,country,whichClass,term,university,Unyshcool,proffession,planType,aid,legacy,activityName,applyingFrom,testSubmit,recentSchool,report,reportDescription,hadtests,hobby,workExperience,addinfo} = req.body;
-    const user = await model.findOne({where:{email}})
+    const {
+      fullName,
+      email,
+      password,
+      phone,
+      age,
+      country,
+      whichClass,
+      term,
+      university,
+      Unyshcool,
+      proffession,
+      planType,
+      aid,
+      legacy,
+      activityName,
+      applyingFrom,
+      testSubmit,
+      recentSchool,
+      report,
+      reportDescription,
+      hadtests,
+      hobby,
+      workExperience,
+      addinfo,
+      area,
+    } = req.body;
+    const user = await model.findOne({ where: { email } });
 
-  const isNull = Object.values({fullName,email,password,phone,age,country,whichClass,term,university,Unyshcool,proffession,planType,aid,legacy,activityName,applyingFrom,testSubmit,recentSchool,report,reportDescription,hadtests,hobby,workExperience,addinfo}).map((el,index)=>{
-         if(el==null){
-          return index
+    if (whichClass == 10) {
+      const isNull = Object.values({
+        fullName,
+        email,
+        password,
+        phone,
+        age,
+        country,
+        whichClass,
+        term,
+        university,
+        Unyshcool,
+        proffession,
+        planType,
+        aid,
+        legacy,
+        activityName,
+        applyingFrom,
+        testSubmit,
+        recentSchool,
+        report,
+        reportDescription,
+        hadtests,
+        hobby,
+        workExperience,
+        addinfo
+      }).map((el, index) => {
+        if (el == null) {
+          return index;
         }
         return null;
-  })
-  const indexArr = isNull.filter((el)=>{
-  return el>0
-})
-let whichIsNull= [];
-indexArr.map((index)=>{
- var arr = Object.keys({fullName,email,password,phone,age,country,whichClass,term,university,Unyshcool,proffession,planType,aid,legacy,activityName,applyingFrom,testSubmit,recentSchool,report,reportDescription,hadtests,hobby,workExperience,addinfo}).filter((element,i)=>{
-    return index==i;
-})
-  whichIsNull.push(arr.join())
- })
-  if(whichIsNull.length>=1){
-    return res.json(whichIsNull);
-  } 
+      });
+      const indexArr = isNull.filter((el) => {
+        return el > 0;
+      });
+      let whichIsNull = [];
+      indexArr.map((index) => {
+        var arr = Object.keys({
+          fullName,
+          email,
+          password,
+          phone,
+          age,
+          country,
+          whichClass,
+          term,
+          university,
+          Unyshcool,
+          proffession,
+          planType,
+          aid,
+          legacy,
+          activityName,
+          applyingFrom,
+          testSubmit,
+          recentSchool,
+          report,
+          reportDescription,
+          hadtests,
+          hobby,
+          workExperience,
+          addinfo
+        }).filter((element, i) => {
+          return index == i;
+        });
+        whichIsNull.push(arr.join());
+      });
+      if (whichIsNull.length >= 1) {
+        return res.json(whichIsNull);
+      }
+    } else {
+      console.log("s+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+      const isNull = Object.values({
+        fullName,
+        email,
+        password,
+        phone,
+        age,
+        country,
+        whichClass,
+        term,
+        university,
+        Unyshcool,
+        proffession,
+        planType,
+        aid,
+        legacy,
+        area
+      }).map((el, index) => {
+        if (el == null) {
+          return index;
+        }
+        return null;
+      });
+      const indexArr = isNull.filter((el) => {
+        return el > 0;
+      });
+      let whichIsNull = [];
+      indexArr.map((index) => {
+        var arr = Object.keys({
+          fullName,
+          email,
+          password,
+          phone,
+          age,
+          country,
+          whichClass,
+          term,
+          university,
+          Unyshcool,
+          proffession,
+          planType,
+          aid,
+          legacy,
+          area
+        }).filter((element, i) => {
+          return index == i;
+        });
+        whichIsNull.push(arr.join());
+      });
+      if (whichIsNull.length >= 1) {
+        return res.json(whichIsNull);
+      }
+    }
+    if (!user) {
+      const hashEmail = bcrypt.hashSync(email, 10);
+      const hashPassword = bcrypt.hashSync(password, 10);
 
-  if(!user){
-    const hashEmail = bcrypt.hashSync(email, 10);
-    const hashPassword = bcrypt.hashSync(password, 10);
-
-    const item = await model.create({fullName,email,password:hashPassword,phone,age,country,whichClass,term,university,Unyshcool,proffession,planType,aid,legacy,activityName,applyingFrom,testSubmit,recentSchool,report,reportDescription,hadtests,hobby,workExperience,addinfo}); 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD,
-      },  
-    });
-    const mailOptions = {
-      from: "davit.manukyan.d@tumo.org",
-      to: email,
-      subject: "verification",
-      html: "<a href='http://164.90.224.111/verify?email=" +hashEmail+ "?id="+item.id+"'>click here</a>",
-    };
-
+      const item = await model.create({
+        fullName,
+        email,
+        password: hashPassword,
+        phone,
+        age,
+        country,
+        whichClass,
+        term,
+        university,
+        Unyshcool,
+        proffession,
+        planType,
+        aid,
+        legacy,
+        activityName,
+        applyingFrom,
+        testSubmit,
+        recentSchool,
+        report,
+        reportDescription,
+        hadtests,
+        hobby,
+        workExperience,
+        addinfo,
+      });
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.EMAIL,
+          pass: process.env.PASSWORD,
+        },
+      });
+      const mailOptions = {
+        from: "davit.manukyan.d@tumo.org",
+        to: email,
+        subject: "verification",
+        html:
+          "<a href='http://164.90.224.111/verify?email=" +
+          hashEmail +
+          "?id=" +
+          item.id +
+          "'>click here</a>",
+      };
 
       transporter.sendMail(mailOptions);
-  return res.status(200).json("email is sent!")
-  }else{
-    return res.json("user alredy exit")
-  }} catch (error) {
-    console.log(error),"++++++++++++++++++++++++++++++++++++++++++++++++";
+      return res.status(200).json("email is sent!");
+    } else {
+      return res.json("user alredy exit");
+    }
+  } catch (error) {
+    console.log(error), "++++++++++++++++++++++++++++++++++++++++++++++++";
   }
 };
 
