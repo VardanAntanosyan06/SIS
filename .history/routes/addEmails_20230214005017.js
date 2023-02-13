2dc5ae5f09e0e5733de8c0332259cc29e003063c
VardanAntanosyan06 <vantanosyan99@gmail.com>
2023-02-14 00:52:24 +0400

    get univeristy

---
+++ .history/routes/addEmails_20230214005017.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+const controller = require("../controllers/addEmailsController")
+
+router.post("/",controller.addEmail)
+router.patch("/",controller.verifyEmail)
+router.patch("/",controller.verifyEmail)
+
+
+module.exports = router;
