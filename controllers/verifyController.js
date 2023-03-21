2cbbf27c2ca6e9eac86257178490d17056883dc0
VardanAntanosyan06 <vantanosyan99@gmail.com>
2023-03-21 18:25:54 +0400

    email

--- controllers/verifyController.js
+++ controllers/verifyController.js
@@ -2,36 +2,38 @@ const model = require("../models").UserEmails;
 const jwt = require("jsonwebtoken");
 const moment = require("moment");
 
-
-const verify = async (req,res)=>{
-    const token = req.query.token
-    const item = await model.findOne({where:{token}})
-    try {
-        if(item){
-        if(item.isVerified === false && (moment().diff(item.tokenCreatedAt,'hours')<=24)){
-            item.isVerified = true;
-            item.token = jwt.sign(
-                {user_id: item.id, email:item.email},
-                process.env.SECRET
-            );
-            item.tokenCreatedAt = null;
-            await item.save();
-            return res.status(200).json({success:true})
-        }else{  
-            item.token = jwt.sign(
-                {user_id: item.id, email:item.email},
-                process.env.SECRET
-            );
-            return res.status(403).json("token timeout!")
-        }
+const verify = async (req, res) => {
+  const token = req.query.token;
+  const item = await model.findOne({ where: { token } });
+  try {
+    if (item) {
+      if (
+        item.isVerified === false &&
+        moment().diff(item.tokenCreatedAt, "hours") <= 24
+      ) {
+        item.isVerified = true;
+        item.token = jwt.sign(
+          { user_id: item.id, email: item.email },
+          process.env.SECRET
+        );
+        item.tokenCreatedAt = null;
+        await item.save();
+        return res.status(200).json({ success: true });
+      } else {
+        item.token = jwt.sign(
+          { user_id: item.id, email: item.email },
+          process.env.SECRET
+        );
+        item.tokenCreatedAt = moment();
+        return res.status(403).json("token timeout!");
+      }
     }
-    return res.status(404).json("user does not exist")
-
-} catch (error) {
+    return res.status(404).json("user does not exist");
+  } catch (error) {
     console.log(error);
-}
-}
+  }
+};
 
 module.exports = {
-    verify
-}   
+  verify,
+};
