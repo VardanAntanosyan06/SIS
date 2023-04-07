7c91baa75ef3b936affa2e34b1895a5af689a2fe
VardanAntanosyan06 <vantanosyan99@gmail.com>
2023-04-07 17:58:53 +0400

    commit for email

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use(fileUpload());
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 app.use(logger("dev"));
-app.use(express.json());
+app.use(express.json({limit:"50mb"}));
 app.use(express.urlencoded({ extended: false,limit:"50mb"}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
