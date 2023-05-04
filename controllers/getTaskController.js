5b4554292ada64e1445422ae43639caa82dfb75a
VardanAntanosyan06 <vantanosyan99@gmail.com>
2023-05-04 17:52:45 +0400

    commit for email UI bug

--- controllers/getTaskController.js
+++ controllers/getTaskController.js
@@ -326,8 +326,8 @@ const taksDescription = async (req, res) => {
     var duration = moment.duration(moment(now).diff(startDate));
     var days = Math.ceil(duration.asDays());
  
-    taskDesc = taskDesc.sort((a,b)=>a.point+b.point)
-    console.log(taskDesc);
+    taskDesc = taskDesc.sort((a,b)=>a.point-b.point)
+    taskDesc = taskDesc.reverse()
     return res.json({
       taskDesc,
       currentDay: days,
