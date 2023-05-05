286c45d57edfc95512970dc84d3fa5d8af6789ab
VardanAntanosyan06 <vantanosyan99@gmail.com>
2023-05-05 14:10:22 +0400

    commit for email UI bug

--- controllers/resetPasswordCntroller.js
+++ controllers/resetPasswordCntroller.js
@@ -94,7 +94,7 @@ const sendMail = async (req, res) => {
                 Seems like you forget your password for SIS Progress. If this is true, click below to reset your password. 
                 </p>
                 <a href="https://sisprogress.com/changepassword?token=${user.token}">
-                <button>
+                <button class="cursor-pointer">
                 Reset My Password
                 </button>
                   
@@ -224,6 +224,9 @@ const sendMail = async (req, res) => {
               }
             </style>
         
+
+            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
           </body>
         </html>
         `,
