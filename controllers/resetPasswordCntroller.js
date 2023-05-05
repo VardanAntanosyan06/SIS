436affa961d14db500f8cd3a23f17364b9b828ab
VardanAntanosyan06 <vantanosyan99@gmail.com>
2023-05-05 13:21:50 +0400

    commit for email UI bug

--- controllers/resetPasswordCntroller.js
+++ controllers/resetPasswordCntroller.js
@@ -1,5 +1,5 @@
 const UserEmails = require("../models").UserEmails;
-const UserModel = require("../models").Users
+const UserModel = require("../models").Users;
 const bcrypt = require("bcrypt");
 const nodemailer = require("nodemailer");
 const jwt = require("jsonwebtoken");
@@ -35,48 +35,209 @@ const sendMail = async (req, res) => {
           pass: process.env.PASSWORD,
         },
       });
+
       const mailOptions = {
         from: "info@sisprogress.com",
         to: email,
-        subject: "Password Reset",
-        html: `<center>
-       <img src='cid:logo' style="width:450px;height:250px;" >
-       <h2>Password Reset</h2>
-       <p>
-       Seems like you forget your password for SIS Progress. If this is true, click below to reset your password. 
-       </p>
-       <br>
-       <br>
-        <button style="background-color: blue;
-        border: none;
-        border-radius:20px;
-        color: white;
-        padding: 15px 32px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        ">
-          <a href='https://sisprogress.com/changepassword?token=${user.token}'
-          style="color:#fff;text-decoration-line: none;font-size:20px;">Reset My Password</a>
-        </button>
-       <br>
-       <br>
-        <b>if you  did not  forget your password you can safely ignore this email.</b>
-        <br>
-        <br>
-        <a href='https://sisprogress.com/changepassword?token=${user.token}'>https://sisprogress.com/changepassword?token=${user.token}</a>   
-        <br>
-        </center>
-          `,
+        subject: "verification",
+        html: `<!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Document</title>
+            <link
+              href="https://fonts.googleapis.com/css?family=Poppins"
+              rel="stylesheet"
+            />
+          </head>
+          <body>
+            <center>
+              <h1
+                style="
+                  
+                  font-style: normal;
+                  font-weight: 600;
+                  font-size: 32px;
+                  line-height: 48px;
+                "
+              >
+              Password Reset
+              </h1>
+              
+              <img src='cid:logo' style="width:450px;height:250px;" >
+              <div style="width: 70%">
+                <p
+                  style="
+                    
+                    font-style: normal;
+                    font-weight: 600;
+                    font-size: 22px;
+                    line-height: 48px;
+                  "
+                >
+                Seems like you forget your password for SIS Progress. If this is true, click below to reset your password. 
+                </p>
+                <a href="https://sisprogress.com/message?token=${user.token}">
+                  <button
+                    style="
+                      width: 130px;
+                      height: 40px;
+                      background: #425dac;
+                      border-radius: 5px;
+                      border:none;  
+                      font-style: normal;
+                      font-weight: 500;
+                      font-size: 18px;
+                      line-height: 27px;
+                      color: #ffffff;
+                      cursor:pointer;
+                    "
+                  >
+                  Reset My Password
+                  </button>
+                </a>
+              </div>
+              <div
+                style="width: 70%;margin-top: 30px"
+                style="border-top: 1px solid #d4d4d4;border-bottom: 1px solid #d4d4d4;"
+              >
+                <p style="font-size: 20px; line-height: 30px;text-align:left;"
+                  >If the button is not working please use the link below:
+                  <a
+                    href="https://sisprogress.com/message?token=${user.token}"
+                    style="color: #425dac;text-align:left;font-size:18px;"
+                    >https://sisprogress.com/message?token=${user.token}</a
+                  >
+                </p>
+              </div>
+              <div
+              style="
+                width: 70%;
+                margin-top: 25px;
+                margin-bottom: 25px;
+                border-top: 1px solid #d4d4d4;
+                border-bottom: 1px solid #d4d4d4;
+                ">
+              <p
+              style="
+              display:flex;
+              
+              font-weight: 500;
+              font-size: 18px;
+              line-height: 27px;
+              color: #646464;
+              text-align: left;
+             "
+              >
+                Regards,
+              </p>
+              <div style="display:flex;">
+              <img src="cid:SISlogo" alt="" width="90px" height="47px"/>
+              </div>
+              <p
+              style="
+              display:flex;
+              
+              font-weight: 500;
+              font-size: 18px;
+              line-height: 27px;
+              color: #646464;
+              text-align: left;
+             "
+              >
+                You have expressed interest in or supported SIS Progress.
+              </p>
+              <p
+              style="
+              display:flex;
+              
+              font-weight: 500;
+              font-size: 18px;
+              line-height: 27px;
+              color: #646464;
+              text-align: left;
+             "
+              >
+                Our mailing address is:
+                <a
+                  href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=GTvVlcSGLdqJpVPMGCFHQZXRljQjDczTJpzSxnxCrfjsQwRhFPPVRncHqjzjPlgcqRRZhgWPGZwJB"
+                  style="color: #425dac;"
+                  >info@sisprogress.com</a
+                >
+              </p>
+              <p
+                style="
+                display:flex;
+                
+                font-weight: 500;
+                font-size: 18px;
+                line-height: 27px;
+                color: #646464;
+                text-align: left;
+               "
+              >
+                Want to change how you receive these emails?
+              </p>
+              <p
+                style="
+                
+                font-weight: 500;
+                font-size: 18px;
+                line-height: 27px;
+                color: #646464;
+                text-align: left;
+               "
+              >
+                You can update your
+                <a href="" style="color: #425dac;">references</a>
+                and 
+                <a href="" style="color: #425dac;"
+                  > unsubscribe.</a
+                >
+              </p>
+            </div>
+            <div style="width:70%">
+            <p style="
+            
+            font-style: normal;
+            font-weight: 500;
+            font-size: 18px;
+            line-height: 27px;
+            color: #646464;
+            text-align: center;
+            margin-top:15px;
+            ">© 2023 SIS Progress, All rights reserved</p></div>
+            </center>
+            <style>
+              div {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+              }
+              p {
+                text-align: left;
+              }
+            </style>
+        
+          </body>
+        </html>
+        `,
         attachments: [
           {
             filename: "Password.png",
             path: "./controllers/Password.png",
             cid: "logo",
           },
+          {
+            filename: "SISlogo.png",
+            path: "controllers/SISlogo.png",
+            cid: "SISlogo",
+          },
         ],
       };
-
       transporter.sendMail(mailOptions);
       return res.status(200).json("email is sent!");
     }
