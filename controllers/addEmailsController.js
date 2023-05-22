2183aba60f27de306c794aad934a7172b20c32d9
VardanAntanosyan06 <vantanosyan99@gmail.com>
2023-05-22 18:01:04 +0400

    task bug

--- controllers/addEmailsController.js
+++ controllers/addEmailsController.js
@@ -718,17 +718,10 @@ const isEmailFree = async (req, res) => {
   try {
     let { email } = req.query;
     email = email.toLowerCase();
-    let allUserEmails = await UserModel.findAll({
-      include: [
-        { model: UserEmails, where: { email } },
-        DeletedUsers,
-      ],
+
+    const user = await UserModel.findOne({
+      include: [{ model: UserEmails, where: { email } }, DeletedUsers],
     });
-  
-    allUserEmails = allUserEmails.filter(
-      (e) => e.DeletedUser === null || e.DeletedUser.isVerified === false
-    ); 
-    const user = allUserEmails.sort((a,b)=>a.id-b.id)[allUserEmails.length-1]
     if (!user || (user.DeletedUser && user.DeletedUser.isVerified === true)) {
       return res.status(200).json("Free");
     }
